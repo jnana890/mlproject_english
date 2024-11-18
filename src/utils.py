17591f99b540b3ef86a4ef8c9869e5eea17de84b
jnana890 <sahoojnana3002@gmail.com>
2024-11-18 11:16:48 +0530

    prediction pipeline

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import dill
+import pickle
 
 import numpy as np
 import pandas as pd
@@ -51,3 +52,12 @@ def evaluate_model(X_train,y_train,X_test,y_test,models,param):
         return report
     except Exception as e:
         raise CustomException(e,sys)
+    
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
